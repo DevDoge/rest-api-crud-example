15d362a83b14830c21b6e1879fe2431b97b4ab24
DevDoge <lhuthefranc@hotmail.com>
2022-03-26 11:03:56 -0300

    Initial commit

---
+++ src/main/java/com/crud/example/restapicrudexample/CarroRepository.java
@@ -0,0 +1,7 @@
+package com.crud.example.restapicrudexample;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarroRepository extends JpaRepository<Carro, Long> {
+
+}
